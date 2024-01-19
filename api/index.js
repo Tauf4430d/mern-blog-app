bba797529199f78031a951c67f5e3821868334f1
Tauf4430d <amee4430d@gmail.com>
2024-01-19 23:13:17 +0530

    Connect to the DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB).then(()=> console.log('mongo connected')).catch((err) => console.log(err))
 const app = express()
 
 app.listen(2000, () => {
