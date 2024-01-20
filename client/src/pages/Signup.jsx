6a56294032e96c1e2b554b22c2f180e3877f7513
Tauf4430d <amee4430d@gmail.com>
2024-01-20 18:09:40 +0530

    Created the Signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
-
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-7">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-4xl text-white">
+              Taufiq's Blog
+            </span>
+          </Link>
+          <p className="text-sm mt-5 ">
+            A Practice Blog App using mern Stack to improve my Skills
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput type="text" placeholder="Email" id="email" />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type="submit">Sign up</Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5 ">
+            <span>Have an account?</span>
+            <Link to='/signin' className="text-blue-500">Sign In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
