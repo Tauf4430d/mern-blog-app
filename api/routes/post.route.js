c2feb4c58068f0f2b8a8b656fa8837d679b6684c
Tauf4430d <amee4430d@gmail.com>
2024-02-03 11:09:51 +0530

    Created the getPosts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const {verifyToken} = require('../utils/verifyUser')
-const { create } = require('../controllers/post.controller.js')
+const { create, getPosts } = require('../controllers/post.controller.js')
 const router = express.Router()
 
 router.post('/create', verifyToken, create)
-
+router.get('/getPosts', getPosts)
 module.exports = router
