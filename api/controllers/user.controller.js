a87b7d2b44311134bc00096535a342266b8ee8bd
Tauf4430d <amee4430d@gmail.com>
2024-02-06 09:37:53 +0530

    Created get Users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -62,9 +62,46 @@ const signOut = async (req, res, next) => {
     }
 }
 
+const getUsers = async (req, res, next) => {
+    if(!req.user.isAdmin) {
+        return next(errorHandler(403, "You ar not allowed to see all users"))
+    }
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0
+        const limit = parseInt(req.query.limit) || 9
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1
+        const users = await USER.find()
+        .sort({ createdAt: sortDirection })
+        .skip(startIndex)
+        .limit(limit)
+        const usersWithoutPassword = users.map((user) => {
+            const {password, ...rest} = user._doc
+            return rest
+        })
+        const totalUsers = await USER.countDocuments()
+        const now = new Date()
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth()-1,
+            now.getDate()
+        )
+        const lastMonthUsers = await USER.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        })
+        res.status(200).json({
+            users : usersWithoutPassword,
+            totalUsers,
+            lastMonthUsers,
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     test,
     updateUser,
     deleteUser,
-    signOut
+    signOut,
+    getUsers
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { test, updateUser, deleteUser, signOut } = require('../controllers/user.controller')
+const { test, updateUser, deleteUser, signOut, getUsers } = require('../controllers/user.controller')
 const { verifyToken } = require('../utils/verifyUser')
 router.get('/test', test)
 router.put('/update/:userId', verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
 router.post('/signout', signOut)
+router.get('/getUsers', verifyToken, getUsers)
 module.exports = router
