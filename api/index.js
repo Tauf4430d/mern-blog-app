b6b9b778cb02e6b5928ec7ba806241ce992867e2
Tauf4430d <amee4430d@gmail.com>
2024-03-03 22:15:32 +0530

    added some changes to deploy

--- api/index.js
+++ api/index.js
@@ -6,9 +6,11 @@ const userRouter = require('./routes/user.route.js')
 const authRouter = require('./routes/auth.route.js')
 const postRouter = require('./routes/post.route.js')
 const commentRouter = require('./routes/comment.route.js')
+const path = require('path')
 const cookieParser = require('cookie-parser')
 dotenv.config()
 mongoose.connect(process.env.MONGO_DB).then(()=> console.log('mongo connected')).catch((err) => console.log(err))
+const __dirname = path.resolve()
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
@@ -17,6 +19,11 @@ app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/post', postRouter)
 app.use('/api/comment', commentRouter)
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500
     const message = error.message || 'Internal Server error'
